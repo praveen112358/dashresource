bd797034a995b9baae358c4ec35c5c57a9e27336
pkr112358 <pkr112358@github.com>
2020-10-25 12:41:02 +0530

    1

---
+++ webscraping_1.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 21 23:08:38 2020
+
+@author: ADMIN
+"""
+
+import os
+import requests
+from urllib.parse import urljoin
+from bs4 import BeautifulSoup
+
+url = "http://www.gatsby.ucl.ac.uk/teaching/courses/ml1-2016.html"
+
+#If there is no such folder, the script will create one automatically
+folder_location = r'E:\webscraping'
+if not os.path.exists(folder_location):os.mkdir(folder_location)
+
+response = requests.get(url)
+soup= BeautifulSoup(response.text, "html.parser")     
+for link in soup.select("a[href$='.pdf']"):
+    #Name the pdf files using the last portion of each link which are unique in this case
+    filename = os.path.join(folder_location,link['href'].split('/')[-1])
+    with open(filename, 'wb') as f:
+        f.write(requests.get(urljoin(url,link['href'])).content)
